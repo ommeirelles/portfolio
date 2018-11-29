d814496519b85250570e20db5c7291ad9da545c9
Otavio Meirelles <otaviomeirelles@MacBook-Air-de-Otavio.local>
2018-11-29 01:00:05 -0200

    Mudada paginas iniciis

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './style/base/_base.sass';
+import App from './pages/main/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
