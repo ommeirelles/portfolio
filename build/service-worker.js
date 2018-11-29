947054fd4ab1121f8e5f51fdc0704c9d61c3408c
Otavio Meirelles <otaviomeirelles@MacBook-Air-de-Otavio.local>
2018-11-29 01:15:29 -0200

    gh-pages

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4f22360059f357e032f804e962c8b877.js"
+  "/portfolio/precache-manifest.32ad1e2bc075df0e2ac5dee903db982c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
