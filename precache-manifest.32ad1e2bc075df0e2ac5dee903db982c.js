fb9119fedceb48263e097c43434cc63fd8b09a85
Otavio Meirelles <otaviomeirelles@MacBook-Air-de-Otavio.local>
2018-11-29 01:15:29 -0200

    gh-pages

---
+++ precache-manifest.32ad1e2bc075df0e2ac5dee903db982c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c182a712485fd7bd0e91fdbbe8842ca9",
+    "url": "/portfolio/static/media/i.c182a712.jpg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "4840530b134a50a75992",
+    "url": "/portfolio/static/js/main.4840530b.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/portfolio/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "4840530b134a50a75992",
+    "url": "/portfolio/static/css/main.d61a3677.chunk.css"
+  },
+  {
+    "revision": "8197e3296e2f4140c3b3187f1115d731",
+    "url": "/portfolio/index.html"
+  }
+];
